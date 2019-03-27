f7d74f005db186a7bef190ef4da26fec5233833a
jesusarockiasankaran <jesusarockias@syncfusion.com>
2019-03-27 14:58:28 +0530

    committing area sample in core

---
+++ CoreArea/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿// Write your JavaScript code.
+
+function onChange(args) {
+    var scheduleObj = document.getElementById('schedule').ej2_instances[0];
+    scheduleObj.selectedDate = args.value;
+    scheduleObj.dataBind();
+}
